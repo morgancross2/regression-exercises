aadb1a97d345bc6e84be9f5081db6bb3f772e56a
Morgan Cross <morgan.e.cross2@gmail.com>
2022-08-30 16:55:26 -0500

    completed scaling exercises

--- wrangle.py
+++ wrangle.py
@@ -4,6 +4,7 @@ import os
 import env
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler
 
 
 def wrangle_zillow():
@@ -18,17 +19,7 @@ def wrangle_zillow():
     if os.path.isfile(filename):
         df = pd.read_csv(filename).iloc[:,1:]
     else:
-        q1 = '''SELECT bedroomcnt, 
-                    bathroomcnt, 
-                    calculatedfinishedsquarefeet,
-                    taxvaluedollarcnt,
-                    yearbuilt,
-                    taxamount,
-                    fips
-                 FROM properties_2016
-                 WHERE propertylandusetypeid = 261
-             ;'''
-        q2 = '''SELECT bedroomcnt, 
+        q = '''SELECT bedroomcnt, 
                     bathroomcnt, 
                     calculatedfinishedsquarefeet,
                     taxvaluedollarcnt,
@@ -38,32 +29,73 @@ def wrangle_zillow():
                  FROM properties_2017
                  WHERE propertylandusetypeid = 261
              ;'''
-        df1 = pd.read_sql(q1, conn('zillow'))
-        df2 = pd.read_sql(q2, conn('zillow'))
-        df = pd.concat([df1,df2])
-
+        df = pd.read_sql(q, conn('zillow'))
+        
         df.to_csv(filename)
         
         
     # Prepare
-    df = df.dropna(how='any')
-    df.bedroomcnt = df.bedroomcnt.astype('int64')
-    df.calculatedfinishedsquarefeet = df.calculatedfinishedsquarefeet.astype('int64')
-    df.taxvaluedollarcnt = df.taxvaluedollarcnt.astype('int64')
-    df.yearbuilt = df.yearbuilt.astype('int64')
-    df.fips = df.fips.astype('int64')
+    cols = ['bedroomcnt', 'bathroomcnt', 'calculatedfinishedsquarefeet','taxvaluedollarcnt','taxamount']
     
-    df.columns = ['beds', 'baths', 'sqft', 'price', 'built', 'taxes', 'location']
+    for col in cols:
+        q1,q3 = df[col].quantile([.25,.75])
+        iqr = q3-q1
+        upper = q3 + 1.5*iqr
+        lower = q1 - 1.5*iqr
+
+        df = df[(df[col] > lower) & (df[col] < upper)]
+    
+    df.columns = ['beds', 'baths', 'sqft', 'tax_value', 'built', 'taxes', 'location']
     df = df.drop(index=df[(df.sqft < 300)].index)
     
     return df
 
-def split_data(df, target):
+def split_data(df):
     '''
     Takes in a dataframe and target (as a string). Returns train, validate, and test subset 
     dataframes with the .2/.8 and .25/.75 splits to create a final .2/.2/.6 split between datasets
     '''
-    train, test = train_test_split(df, test_size = .2, random_state=123, stratify=df[target])
-    train, validate = train_test_split(train, test_size = .25, random_state=123, stratify=train[target])
+    train, test = train_test_split(df, test_size = .2, random_state=123)
+    train, validate = train_test_split(train, test_size = .25, random_state=123)
     
     return train, validate, test
+
+
+def impute_mode(train, validate, test, col):
+    '''
+    Takes in train, validate, and test as dfs, and column name (as string) and uses train 
+    to identify the best value to replace nulls in embark_town
+    
+    Imputes the most_frequent value into all three sets and returns all three sets
+    '''
+    imputer = SimpleImputer(strategy='most_frequent')
+    imputer = imputer.fit(train[[col]])
+    train[[col]] = imputer.transform(train[[col]])
+    validate[[col]] = imputer.transform(validate[[col]])
+    test[[col]] = imputer.transform(test[[col]])
+    
+    return train, validate, test
+
+
+def scale_data(X_train, y_train, X_val, y_val, X_test, y_test):
+    '''
+    This function takes in train, val, test datasets and returns the 
+    MinMaxScalar values in new dataframes.
+    '''
+    scaler = MinMaxScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_val_scaled = scaler.transform(X_val)
+    X_test_scaled = scaler.transform(X_test)
+    
+    X_train_scaled = pd.DataFrame(X_train_scaled, columns=['beds','baths','sqft','built','taxes','location'])
+    X_val_scaled = pd.DataFrame(X_val_scaled, columns=['beds','baths','sqft','built','taxes','location'])
+    X_test_scaled = pd.DataFrame(X_test_scaled, columns=['beds','baths','sqft','built','taxes','location'])
+
+#     train_scaled['built'] = train.built
+#     train_scaled['location'] = train.location
+#     val_scaled['built'] = val.built
+#     val_scaled['location'] = val.location
+#     test_scaled['built'] = test.built
+#     test_scaled['location'] = test.location
+    
+    return X_train_scaled, X_val_scaled, X_test_scaled
